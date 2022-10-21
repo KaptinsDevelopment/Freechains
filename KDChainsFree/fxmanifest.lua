376b804480e471526b564de643b74de8c78eeadd
Kaptins Development <102648425+KaptinsDevelopment@users.noreply.github.com>
2022-10-21 00:13:08 -0700

    Add files via upload

---
+++ KDChainsFree/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_kaptindev.meta',
+  'mp_f_freemode_01_mp_f_kaptindev.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_kaptindev.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_kaptindev.meta'
